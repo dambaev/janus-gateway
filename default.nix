64ad1904f5d24cf1df4ba8e4ab4d6e238fedfa5f
Dambaev Alexander <ice.redmine@gmail.com>
2019-08-19 12:40:29 +0800

    add initial default.nix

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetgit }:
+
+let
+  tag = "v0.7.3"; # git tag
+in
+stdenv.mkDerivation rec {
+  name = "janus-gateway-${tag}";
+  version = tag;
+  src = fetchgit {
+    url = "https://github.com/meetecho/janus-gateway";
+    branchName = tag;
+  };
+
+  buildInputs = [ jansson libconfig libnice openssl glib pkg-config gengetopt libsrtp sofia_sip libopus ];
+  meta = with stdenv.lib; {
+    description = "Janus WebRTC Server";
+    homepage    = "https://janus.conf.meetecho.com/";
+    license     = licenses.gpl3Plus;
+    platforms   = platforms.linux;
+    maintainers = [];
+  };
+}
